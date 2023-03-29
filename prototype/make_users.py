c28afb6c4fd02105e4b5f76a11ed24d868bb6181
CSE-Shaco <kjm6460@inha.edu>
2023-03-29 20:23:16 +0900

    edit dir

--- prototype/make_users.py
+++ prototype/make_users.py
@@ -2,7 +2,7 @@ import json
 
 
 def do():
-    with open('members.json', 'r', encoding='utf-8') as f:
+    with open('../data/members.json', 'r', encoding='utf-8') as f:
         users = json.load(f)
     f.close()
     for elem in users:
