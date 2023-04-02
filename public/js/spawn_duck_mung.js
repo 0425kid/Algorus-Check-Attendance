605ed0f95f4b60d39103a4b6b255f2e5bb65f7b4
0425kid <0425kid@gmail.com>
2023-04-02 22:04:04 +0900

    아그덕 아그멍 스폰 코드 분리

---
+++ public/js/spawn_duck_mung.js
@@ -0,0 +1,29 @@
+let img_container = document.getElementById('img_container');
+
+let rotate_num = Math.floor(Math.random() * 6) + 1
+console.log(rotate_num);
+
+for(let i=1;i<8;i++){
+    let duck = document.createElement('img');
+    duck.src='src/agduck.png'
+    duck.classList.add('agDuck', 'agDuck'+i);
+
+    if (i == rotate_num) {
+        duck.classList.add('rotate');
+    }
+    img_container.appendChild(duck);
+}
+
+rotate_num = Math.floor(Math.random() * 5) + 1
+console.log(rotate_num);
+
+for(let i=1;i<7;i++){
+    let mung = document.createElement('img');
+    mung.src='src/agmung.png'
+    mung.classList.add('agMung', 'agMung'+i);
+
+    if (i == rotate_num) {
+        mung.classList.add('rotate');
+    }
+    img_container.appendChild(mung);
+}
