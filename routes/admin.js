3e91b584702f57c8222e9f70db02203fa6d439f6
CSE-Shaco <kjm6460@inha.edu>
2023-04-03 22:08:05 +0900

    updating for user attendance

--- routes/admin.js
+++ routes/admin.js
@@ -74,7 +74,45 @@ router.get('/updateStudents', async (req,res)=>{
     }
 });
     
-    
+router.get('/:week', async (req,res)=>{
+    const week_num = req.params.week;
+    console.log(`${week_num}주차 출석체크`);
+    const json = require(`../prototype/check${week_num}.json`);
+
+    const pg = new postgresql()
+    await pg.connect()
+
+    pg.client.query(
+        `
+        DELETE FROM attendance WHERE week_number = ${week_num};
+        `
+    );
+
+    //json에 있는 데이터대로 새로 출석체크
+    try {
+        await Promise.all(json.map((element) =>
+            pg.client.query(
+                `
+                INSERT INTO attendance(week_number, student_id, student_name, attendance_status)
+                VALUES($1, $2, $3, $4)
+                `,
+                [week_num, element.s_id, element.name, element.atnd]
+            )
+        ));
+        res.send(
+            `
+            <script>
+            alert("Success"); 
+            </script>
+            `
+        );
+    } catch (error) {
+        let errormsg = error.severity + ' : ' + error.code + ' : ' + error.detail;
+        res.status(400).send(errormsg);
+    } finally {
+        await pg.disconnect();
+    }
+});
 
 
 module.exports = router
