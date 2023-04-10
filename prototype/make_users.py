386d21962ddb80677937f6722c3d013c16bfbb59
CSE-Shaco <kjm6460@inha.edu>
2023-04-10 22:19:04 +0900

    update for cron on ubuntu

--- prototype/make_users.py
+++ prototype/make_users.py
@@ -3,7 +3,7 @@ import json
 
 # 기존 유저 목록에 출석 상태 추가 후 반환
 def do():
-    with open('../data/members.json', 'r', encoding='utf-8') as f:
+    with open('/home/ubuntu/Algorus-Check-Attendance/data/members.json', 'r', encoding='utf-8') as f:
         users = json.load(f)
     f.close()
     for elem in users:
